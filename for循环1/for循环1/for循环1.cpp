d1590b90461f30385009ac18645dc570ebf28d0f
kell1245 <3314154247@qq.com>
2025-01-07 15:17:04 +0800

    for循环学习

---
+++ for循环1/for循环1/for循环1.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+	for (int i = 1;i <= 100;i++)
+	{
+		cout << i << endl;
+}
+	for (int i = 800;i <= 1000;i++ )
+	{
+		cout << i << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
